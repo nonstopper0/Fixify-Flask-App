06de62faf855bdb197d7cd0b97074ecb1bd7162e
Joe Hendy <jahendy12@gmail.com>
2020-01-24 15:16:16 -0700

    first commit

---
+++ models.py
@@ -0,0 +1,37 @@
+# import datetime
+# from peewee import *
+# from flask_login import UserMixin
+
+DATABASE = PostgresqlDatabase('fixify_app')
+
+class Mechanic(UserMixin, Model):
+    username = CharField(unique = True)
+    email = CharField(unique = True)
+    password = CharField()
+
+    class Meta:
+        database = DATABASE
+
+class User(Model):
+    username = CharField(unique = True)
+    email = CharField(unique = True)
+    password = CharField()
+    
+
+    class Meta:
+        database = DATABASE
+
+class Problem(Model):
+    make = CharField()
+    model = CharField()
+    price = CharField()
+
+    class Meta: 
+        database = DATABASE
+
+
+def initialize():
+    DATABASE.connect()
+    DATABASE.create_tables([User, Mechanic, Problem], safe=True)
+    print('TABLES created')
+    DATABASE.close()
