3b7f5ef9925ca9860ab8ec840349846a0ce79c4b
Nathaniel Redmon <52013906+nonstopper0@users.noreply.github.com>
2020-01-27 11:40:21 -0700

    register route created

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, g
+from flask import Flask, jsonify, g, request
 from flask_login import LoginManager
 from flask_cors import CORS
 app = Flask(__name__)
@@ -11,32 +11,33 @@ from resources.mechanic import mechanic
 from resources.user import user
 from resources.problem import problem
 
-@login_manager.user_loader
-def load_user(userid):
-    try:
-        return models.User.get(models.User.id == userid)
-    except models.DoesNotExist:
-        return None
+# @login_manager.user_loader
+# def load_user(userid):
+#     try:
+#         return models.User.get(models.User.id == userid)
+#     except models.DoesNotExist:
+#         return None
 
-@login_manager.unauthorized_handler
-def unauthorized():
-    return jsonify(
-        data = {
-            'error': 'User not logged in.'
-        },
-        status = {
-            'code': 401,
-            'message': 'You must be logged in to access that resource.'
-        }
-    )
+# @login_manager.unauthorized_handler
+# def unauthorized():
+#     return jsonify(
+#         data = {
+#             'error': 'User not logged in.'
+#         },
+#         status = {
+#             'code': 401,
+#             'message': 'You must be logged in to access that resource.'
+#         }
+#     )
 
 CORS(mechanic, origin=['http://localhost:3000'], supports_credentials=True)
 CORS(user, origin=['http://localhost:3000'], supports_credentials=True)
 CORS(problem, origin=['http://localhost:3000'], supports_credentials=True)
+CORS(app, supports_credentials=True)
 
-app.register_blueprint(mechanic, url_prefix='/api/v1/mechanic')
-app.register_blueprint(user, url_prefix='/api/v1/user')
-app.register_blueprint(mechanic, url_prefix='/api/v1/problem')
+app.register_blueprint(mechanic, url_prefix='/mechanic')
+app.register_blueprint(user, url_prefix='/user')
+app.register_blueprint(mechanic, url_prefix='/problem')
 
 
 @app.before_request
@@ -53,6 +54,26 @@ def after_request(response):
 @app.route('/')
 def index():
     return 'hi'
+
+@app.route('/login', methods=['POST']) 
+def login():
+    return "hi"
+
+@app.route('/register', methods=['POST'])
+def register():
+    payload = request.get_json()
+    payload['email'].lower()
+    payload['username'].lower()
+    payload['type'].lower()
+    try:
+        if payload['type'] == 'user':
+            print('user')
+        elif payload['type'] == 'mechanic':
+            print('mechanic')
+        return jsonify(data ={}, status = {"code": 200, "message": "Success"})
+    except:
+        return jsonify(data={}, status={"code": 400, "message": "Error creating the resources"})
+
 
 
 DEBUG = True
