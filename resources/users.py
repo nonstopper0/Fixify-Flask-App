726a201bb3fcfbe36a5c9668fe48d55aa2f2c0d5
Joe Hendy <jahendy12@gmail.com>
2020-01-27 11:13:53 -0700

    did routes for mechanics

---
+++ resources/users.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, jsonify, request
+from playhouse.shortcuts import model_to_dict
+from flask_login import current_user, login_required
+
+import models
+
+mechanic = Blueprint('user', 'user')
